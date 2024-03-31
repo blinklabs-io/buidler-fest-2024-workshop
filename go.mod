e896de4656bbecb6570487649e5d03264ab8f88b
Aurora Gaffney <aurora@agaffney.org>
2024-03-31 17:50:16 -0500

    feat: wallet support
    This provides support for generating a wallet seed phrase or loading an
    existing one from file/env. This also provides the config framework.

--- go.mod
+++ go.mod
@@ -2,9 +2,21 @@ module github.com/blinklabs-io/buidler-fest-2024-workshop
 
 go 1.21
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/blinklabs-io/bursa v0.7.1
+	github.com/joho/godotenv v1.5.1
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
+	github.com/btcsuite/btcutil v1.0.2 // indirect
+	github.com/fivebinaries/go-cardano-serialization v0.0.0-20220907134105-ec9b85086588 // indirect
+	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
---
+++ internal/wallet/wallet.go
@@ -0,0 +1,83 @@
+// Copyright 2024 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wallet
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/blinklabs-io/buidler-fest-2024-workshop/internal/config"
+
+	"github.com/blinklabs-io/bursa"
+)
+
+var globalWallet *bursa.Wallet
+
+func Setup() (*bursa.Wallet, error) {
+	// Return existing wallet instance if available
+	if globalWallet != nil {
+		return globalWallet, nil
+	}
+	// Setup wallet
+	cfg := config.GetConfig()
+	mnemonic := cfg.Wallet.Mnemonic
+	if mnemonic == "" {
+		// Read seed.txt if it exists
+		if data, err := os.ReadFile("seed.txt"); err == nil {
+			slog.Info("read mnemonic from seed.txt")
+			mnemonic = string(data)
+		} else if errors.Is(err, os.ErrNotExist) {
+			mnemonic, err = bursa.NewMnemonic()
+			if err != nil {
+				return nil, err
+			}
+			// Write seed.txt
+			// WARNING: this will clobber existing files
+			f, err := os.Create("seed.txt")
+			if err != nil {
+				return nil, err
+			}
+			l, err := f.WriteString(mnemonic)
+			slog.Debug(fmt.Sprintf("wrote %d bytes to seed.txt", l))
+			if err != nil {
+				f.Close()
+				return nil, err
+			}
+			err = f.Close()
+			if err != nil {
+				return nil, err
+			}
+			slog.Info("wrote generated mnemonic to seed.txt")
+		} else {
+			return nil, err
+		}
+	}
+	wallet, err := bursa.NewWallet(
+		mnemonic,
+		cfg.Network,
+		0, 0, 0, 0,
+	)
+	if err != nil {
+		return nil, err
+	}
+	globalWallet = wallet
+	return globalWallet, nil
+}
+
+func GetWallet() *bursa.Wallet {
+	return globalWallet
+}
